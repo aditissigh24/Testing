2f2a1eb258c5822b4f5a2b3b6a1e663e601a1bdd
Aditi Singh <aditissigh24@gmail.com>
2025-06-19 21:21:31 +0530

    Merge main and resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import StockTable from './StockTable';
+import ComputerModel from './ComputerModel';
 
 function SpeedDial({ speed, max = 50 }) {
   const clamped = Math.min(speed || 0, max);
@@ -89,8 +91,10 @@ function App() {
         >
           Learn React
         </a>
+        <ComputerModel />
         <SpeedDial speed={speed} />
         <p className={speedClass}>Internet speed: {speedText}</p>
+        <StockTable />
       </header>
     </div>
   );
